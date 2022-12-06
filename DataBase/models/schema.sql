5a5d5ace39798b1349e0b35aa4e5ed5ff47e265c
MustafaMHamdan <hamdan.mustafamof@gmail.com>
2022-12-06 16:11:09 +0300

    build Data Base

--- DataBase/models/schema.sql
+++ DataBase/models/schema.sql
@@ -1,2 +1,33 @@
--- Build Your Tables Here --
+ DROP DATABASE Vue_Cart ;
+CREATE DATABASE Vue_Cart;
 
+USE Vue_Cart;
+
+CREATE TABLE users(
+    UserID INT AUTO_INCREMENT NOT NULL,
+    name VARCHAR(255),
+    phone VARCHAR(255),
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255),
+    is_deleted TINYINT DEFAULT 0,
+    PRIMARY KEY (UserID)
+);
+
+CREATE TABLE products (
+    productID INT AUTO_INCREMENT NOT NULL,
+    title VARCHAR(255),
+    price INT,
+    image VARCHAR(255),
+    is_deleted TINYINT DEFAULT 0,
+    PRIMARY KEY (productID)
+);
+
+CREATE TABLE cart(
+    id INT AUTO_INCREMENT NOT NULL,
+    BuyerId INT,
+    productId INT,
+    FOREIGN KEY (BuyerId) REFERENCES users(UserID),
+    FOREIGN KEY (productId) REFERENCES products(productID),
+    is_deleted TINYINT DEFAULT 0,
+    PRIMARY KEY (id)
+);
